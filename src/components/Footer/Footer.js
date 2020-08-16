9ed9091011232c757fd10ca9d96586bac06652ce
Alkiva <fernanda22mr@gmail.com>
2020-08-15 19:38:48 -0500

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const Footer = () => {
-    return (  
-        <p>Aqui va el footer</p>
+    return (
+      <div class="card text-center">
+          <div class="card-footer text-muted">
+              <div class="container-sm ">
+                <div class="row">
+                  <div class="col-sm">
+                    Contacto
+                  </div>
+                  <div class="col-sm">
+                    Aviso Legal
+                  </div>
+                  <div class="col-sm">
+                    MUI
+                  </div>
+              </div>
+            </div>
+        </div>
+      </div>
     );
 }
- 
+
 export default Footer;
