75c49ca20687c5351af95e2d00be1d488a4a19e8
Alkiva <fernanda22mr@gmail.com>
2020-08-17 17:42:54 -0500

    Description

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import imagen from '../../images/MUI_SONORO-04.png';
 
 const Description = () => {
-    return (  
-        <p>La descripcion del museo con logo</p>
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col" >
+            <h1 class="text-center">Museo Urbano Interactivo</h1>
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-sm" >
+            El MUI (Museo Urbano Interactivo) busca ser un espacio fisico y conceptual,
+            en donde se desarrollan personas concientes, capaces de producir un México
+            con futuro en el que llega a ser una alternativa global; a través de las
+            herramientas propias de las industrias creativas de las prácticas artísticas actuales.
+            Lo que hace desde el contexto en el que esta situado.
+          </div>
+          <div class="col-sm">
+            <img className = "img" src = {imagen} className="w-75"/>
+          </div>
+        </div>
+      </div>
     );
 }
- 
+
 export default Description;
