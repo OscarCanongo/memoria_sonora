9a10782f0afaceb56043e26e9c9daa53ee118c4a
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2020-08-24 15:22:45 -0500

    Description correction

--- Frontend/src/components/Description/Description.js
+++ Frontend/src/components/Description/Description.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import imagen from '../../images/MUI_SONORO-04.png';
 import './Description.css';
+import PublicSans from '../../fonts/PublicSans-VariableFont_wght.ttf';
+import SpaceMono from '../../fonts/SpaceMono-Regular.ttf';
 
 const Description = () => {
     return (
-      <div class="container">
+      <div class="container-sm">
         <div class="row">
-          <div class="col" >
+          <div class="col-sm">
             <h1 class="text-center">Museo Urbano Interactivo</h1>
           </div>
-          </div>
+        </div>
+        <div class="container-sm">
           <div class="row">
-          <div class="txt">
+            <div class="col-sm">
+              <div class="txt">
                     <p>El <b>MUI (Museo Urbano Interactivo)</b> busca ser un espacio físico
                     y conceptual, en donde se desarrollan personas conscientes,
                     capaces de producir un México con futuro en el que llega a ser
                     una alternativa global.</p> <p>A través de las herramientas propias
                     de las industrias creativas y las prácticas artísticas actuales.
                     Lo que hace el contexto en que está situado.</p>
+              </div>
+            </div>
           </div>
-          <div class="col-sm">
-            <img  src = {imagen} className="w-75" alt="MuiLogo"/>
+        </div>
+        <div class="container-sm">
+          <div class = "row">
+            <div div class = "col-md-3 ml-md-auto">
+              <img  src = {imagen} class="img-fluid float-right" alt="MuiLogo"/>
+            </div>
           </div>
         </div>
       </div>
