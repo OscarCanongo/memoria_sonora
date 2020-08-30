73600b91af0d08fcd035ddb48615bef8003a0d74
OscarCanongo <a01730443@itesm.mx>
2020-08-30 17:43:56 -0500

    Back Audios

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,17 @@
-const express = require('express');
-const conection= require('./config/db');
+const express = require('express')
+const cors = require('cors');
+const morgan = require('morgan');
 
-//Crear el servidor
+const trackRoutes = require('./routes/audios');
+
 const app = express();
 
-//Conectar a la base de datos
-conection();
+// middlewares
+app.use(morgan('dev'));
+app.use(cors());
 
-//Habilitar express.json
-app.use(express.json({ extended: true}));
+// routes
+app.use('/audios', trackRoutes);
 
-//Puerto del server
-const PORT = process.env.PORT || 4000;
-
-//Importar rutas
-app.use('/audio', require('./routes/audios'));
-
-//Arrancar server
-app.listen(PORT, () => {
-    console.log(`El server esta corriendo en el puerto ${PORT}`);
-});
+app.listen(4000);
+console.log('Server on port', 4000);
