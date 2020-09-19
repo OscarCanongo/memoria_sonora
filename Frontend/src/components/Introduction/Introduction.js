2bb4c842ce70091233768008e8c6d1d7170c1c82
Alkiva <fernanda22mr@gmail.com>
2020-09-19 18:25:17 -0500

    cambio de tamaño logo

--- Frontend/src/components/Introduction/Introduction.js
+++ Frontend/src/components/Introduction/Introduction.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Logo from '../../images/memoria_sonora_logo.png';
 import './Introduction.css';
 import ParticlesBg from 'particles-bg';
-
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 const Header = () => {
     return (
-        <div>
-            <img className="imagen" class="img-fluid" src={Logo} alt="Memoria Sonora Logo" width = "70%"></img>
+        <div  class="d-flex justify-content-center" >
+            <MDBCol md="8"  >
+            <img class="img-fluid"  src={Logo} alt="aligment"></img>
+            </MDBCol>
             <ParticlesBg type="cobweb" bg={true}/>
         </div>
     );
