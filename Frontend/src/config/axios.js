31c0b81b5e31c08d5a256c8e359a01eaac1ec75e
OscarCanongo <a01730443@itesm.mx>
2020-09-19 13:16:46 -0500

    Deploy Back

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL : "http://localhost:4000"
+    baseURL : "https://sheltered-beyond-44611.herokuapp.com/"
 });
 
 export default clienteAxios;
