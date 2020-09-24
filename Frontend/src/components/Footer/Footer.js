59630921f8371390331efc7a97e995d6898ec386
Benny Ruiz Jimenez <A01328177@itesm.mx>
2020-09-24 15:25:23 -0500

    links en el footer

--- Frontend/src/components/Footer/Footer.js
+++ Frontend/src/components/Footer/Footer.js
@@ -1,24 +1,34 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-      <div className="card text-center">
-          <div className="card-footer text-muted">
-              <div className="container-sm ">
-                <div className="row">
-                  <div className="col-sm">
-                    Contacto
-                  </div>
-                  <div className="col-sm">
-                    Aviso Legal
-                  </div>
-                  <div className="col-sm">
-                    MUI
-                  </div>
-              </div>
-            </div>
-        </div>
-      </div>
+    return ( <
+        div className = "card text-center" >
+        <
+        div className = "card-footer text-muted" >
+        <
+        div className = "container-sm " >
+        <
+        div className = "row" >
+        <
+        div className = "col-sm" >
+        <
+        a class = "linkFooter"
+        href = "https://elmui.online/contacto/" > Contacto < /a> < /
+        div > <
+        div className = "col-sm" >
+        <
+        a class = "linkFooter"
+        href = "https://elmui.online/aviso-de-privacidad/" > Aviso de Privacidad < /a> < /
+        div > <
+        div className = "col-sm" >
+        <
+        a class = "linkFooter"
+        href = "https://elmui.online/about-us/" > MUI < /a> < /
+        div > <
+        /div> < /
+        div > <
+        /div> < /
+        div >
     );
 }
 
